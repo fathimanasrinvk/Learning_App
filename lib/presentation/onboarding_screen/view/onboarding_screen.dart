bdf2285b04fa262c01c1f0812ee712d6e6e0693a
fathimathsafa <safahaneefa01@gmail.com>
2024-05-02 13:16:50 +0530

    onboarding screen added

---
+++ lib/presentation/onboarding_screen/view/onboarding_screen.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'package:gaming_app/core/constants/colors.dart';
+// import 'package:introduction_screen/introduction_screen.dart';
+
+// class OnboardingScreen extends StatelessWidget {
+//   const OnboardingScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return IntroductionScreen (
+// globalBackgroundColor: ColorTheme.secondarycolor,
+// pages: [
+//   PageViewModel(
+//     title: "Learn More",
+//     body: "gahsfkjkfjgbjcnkfjfj"
+
+//   )
+// ],
+
+//     );
+//   }
+// }

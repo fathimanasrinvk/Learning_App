de14833e289e4052c8758f901013be17d0d20d68
nasi321 <nasinasri6102@gmail.com>
2024-05-02 15:08:53 +0530

    registrationpage modifiedg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming_app/presentation/home_screen/view/home_screen.dart';
+import 'package:gaming_app/presentation/login_screen/view/login_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      home: LoginScreen()
     );
   }
 }

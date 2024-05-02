ac0f5e31c1a21616fa4dec6abc95bab3b2e87585
nasi321 <nasinasri6102@gmail.com>
2024-05-02 14:29:51 +0530

    conflict solving

--- lib/presentation/login_screen/view/login_screen.dart
+++ lib/presentation/login_screen/view/login_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gaming_app/core/constants/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
-void main(){
-  runApp(MaterialApp(home: LoginScreen(),));
-}
+// void main(){
+//   runApp(MaterialApp(home: LoginScreen(),));
+// }
 
 class LoginScreen extends StatelessWidget {
   final econtroller = TextEditingController();

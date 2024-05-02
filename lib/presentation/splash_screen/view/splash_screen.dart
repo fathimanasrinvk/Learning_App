fc56e3e07e9c92e86a4eabede9d4495910ba61d1
fathimathsafa <safahaneefa01@gmail.com>
2024-05-02 11:29:56 +0530

    splash screen created

---
+++ lib/presentation/splash_screen/view/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:gaming_app/core/constants/colors.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  //  void initState() {
+  //   Timer(Duration(seconds: 5), () {
+  //     Navigator.of(context).pushReplacement(
+  //         MaterialPageRoute(builder: (context) => ()));
+  //   });
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorTheme.maincolor,
+    );
+  }
+}

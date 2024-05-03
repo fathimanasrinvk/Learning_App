f0c828174f7530697f9983737d72daedd39422f4
fathimathsafa <safahaneefa01@gmail.com>
2024-05-03 11:06:47 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gaming_app/presentation/onboarding_screen/view/onboarding_screen.dart';
+import 'package:gaming_app/presentation/splash_screen/view/splash_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnboardingScreen()
+      home: SplashScreen()
     );
   }
 }

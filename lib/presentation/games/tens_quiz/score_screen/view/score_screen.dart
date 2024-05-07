64ff3ea8fe68176c896564176c7678b787f13bc8
fathimathsafa <safahaneefa01@gmail.com>
2024-05-07 14:19:54 +0530

    word puzzle level screen added

--- lib/presentation/games/tens_quiz/score_screen/view/score_screen.dart
+++ lib/presentation/games/tens_quiz/score_screen/view/score_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gaming_app/core/constants/colors.dart';
 
 class TensQuizScoreScreen extends StatelessWidget {
   const TensQuizScoreScreen({super.key});
@@ -7,7 +8,14 @@ class TensQuizScoreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
      var size = MediaQuery.sizeOf(context);
     return Scaffold(
-
+backgroundColor: ColorTheme.maincolor,
+appBar: AppBar(),
+body: Center(
+  child: Column(
+    children: [
+      
+    ],
+  )),
     );
   }
 }

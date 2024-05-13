b8b897901e3d6a3cc8f665184e62aa3cfe1cca64
fathimathsafa <safahaneefa01@gmail.com>
2024-05-13 11:14:36 +0530

    game screen modified

--- lib/presentation/games/gk/level_screen/view/level_screen.dart
+++ lib/presentation/games/gk/level_screen/view/level_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gaming_app/core/constants/colors.dart';
+import 'package:gaming_app/core/constants/global_text_style.dart';
 import 'package:gaming_app/presentation/game_screen/view/game_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,8 +12,7 @@ class LevelScreenGK extends StatelessWidget {
     var size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: Padding(
-        padding:  EdgeInsets.only(top: size.height*0.1
-        ),
+        padding: EdgeInsets.only(top: size.height * 0.1),
         child: Center(
           child: Column(
             children: [
@@ -38,13 +38,7 @@ class LevelScreenGK extends StatelessWidget {
                           backgroundColor: ColorTheme.maincolor,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15))),
-                      child: Text(
-                        "EASY",
-                        style: GoogleFonts.poppins(
-                            color: ColorTheme.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ))),
+                      child: Text("EASY", style: GlobalTextStyles.subTitle4))),
               SizedBox(
                 height: size.height * .1,
               ),
@@ -57,13 +51,8 @@ class LevelScreenGK extends StatelessWidget {
                           backgroundColor: ColorTheme.maincolor,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15))),
-                      child: Text(
-                        "MEDIUM",
-                        style: GoogleFonts.poppins(
-                            color: ColorTheme.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ))),
+                      child:
+                          Text("MEDIUM", style: GlobalTextStyles.subTitle4))),
               SizedBox(
                 height: size.height * .1,
               ),
@@ -76,13 +65,7 @@ class LevelScreenGK extends StatelessWidget {
                           backgroundColor: ColorTheme.maincolor,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15))),
-                      child: Text(
-                        "HARD",
-                        style: GoogleFonts.poppins(
-                            color: ColorTheme.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ))),
+                      child: Text("HARD", style: GlobalTextStyles.subTitle4))),
             ],
           ),
         ),

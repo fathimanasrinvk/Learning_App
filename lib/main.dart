a7e648aff68b0139d8e1d7551b71daa893d2beca
MeAnandCB <anandcb55@gmail.com>
2024-05-22 13:24:10 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming_app/presentation/home_screen/view/home_screen.dart';
-import 'package:gaming_app/presentation/login_screen/view/login_screen.dart';
-import 'package:gaming_app/presentation/onboarding_screen/view/onboarding_screen.dart';
-import 'package:gaming_app/presentation/registration_page/view/registration_screen.dart';
-import 'package:gaming_app/presentation/splash_screen/view/splash_screen.dart';
-import 'package:gaming_app/presentation/translation_screen/view/translation_screen.dart';
-import 'package:gaming_app/presentation/word_screen/view/word_screen.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-       home: HomeScreen(name: 'git',)
-       // home: LoginScreen()
-    // home:SplashScreen(),
-     //home:WordScreen(),
-     //    home:RegistrationScreen(),
-     //    home:OnboardingScreen(),
-     //    home:TranslationScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(
+          name: 'git',
+        )
+        // home: LoginScreen()
+        // home:SplashScreen(),
+        //home:WordScreen(),
+        //    home:RegistrationScreen(),
+        //    home:OnboardingScreen(),
+        //    home:TranslationScreen()
+        );
   }
 }

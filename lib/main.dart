dd66c1caf677d003d2c18ed7ba6f2a5707ea4232
Jishnu998 <jishnukizhakkumpurath@gmail.com>
2024-05-22 10:30:43 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gaming_app/presentation/games/tens_quiz/controller/api.dart';
+import 'package:gaming_app/presentation/games/tens_quiz/controller/tenzquiz_controller.dart';
 import 'package:gaming_app/presentation/games/tens_quiz/level_screen/view/level_screen.dart';
 import 'package:gaming_app/presentation/home_screen/view/home_screen.dart';
 import 'package:gaming_app/presentation/login_screen/view/login_screen.dart';
@@ -7,6 +9,7 @@ import 'package:gaming_app/presentation/onboarding_screen/view/onboarding_screen
 import 'package:gaming_app/presentation/registration_page/view/registration_screen.dart';
 import 'package:gaming_app/presentation/splash_screen/view/splash_screen.dart';
 import 'package:gaming_app/presentation/translation_screen/view/translation_screen.dart';
+import 'package:provider/provider.dart';
 
 void main(){
   runApp(MyApp());
@@ -15,15 +18,21 @@ void main(){
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: HomeScreen()
-       // home: LoginScreen()
-     home:LevelScreenTensQuiz()
-     //SplashScreen(),
-     //    home:RegistrationScreen(),
-     //    home:OnboardingScreen(),
-     //    home:TranslationScreen()
-    );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TenceQuizController()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+         // home:QuizScreen()
+          // home: HomeScreen()
+          // home: LoginScreen()
+         // home:LevelScreenTensQuiz()
+        home:SplashScreen(),
+        //    home:RegistrationScreen(),
+        //    home:OnboardingScreen(),
+        //    home:TranslationScreen()
+      ));
+
   }
 }

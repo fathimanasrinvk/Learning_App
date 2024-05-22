26d591d1605060b7f604026bf4c7f1f3aa6cf05c
Jishnu998 <jishnukizhakkumpurath@gmail.com>
2024-05-22 11:16:53 +0530

    add

--- lib/presentation/games/tens_quiz/quiz datas.dart
+++ lib/presentation/games/tens_quiz/quiz datas.dart
@@ -1,7 +1,188 @@
-List<Map<String,dynamic>> products=[{
-"id":1,
-"name":"product 1",
-"price": 900,
-"description":"Adidas shoes",
-"image":"https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Coop/Shoesupdated/Campus-978x1256_1._SY1050_QL85_.jpg",
-"rating":4.3},
+List<Map<String, dynamic>> beginner = [
+  {
+    "id": 1,
+    "question": "What is the past tense of 'eat'?",
+    "answer": "ate",
+    "options": ["ate", "will eat", "eaten"]
+  },
+  {
+    "id": 2,
+    "question": "What is the present continuous tense of 'go'?",
+    "answer": "going",
+    "options": ["went","going",  "gone"]
+  },
+  {
+    "id": 3,
+    "question": "What is the future tense of 'run'?",
+    "answer": "will run",
+    "options": ["will run", "ran", "running"]
+  },
+  {
+    "id": 4,
+    "question": "What is the present perfect tense of 'drink'?",
+    "answer": "have drunk",
+    "options": [ "drank", "drink","have drunk"]
+  },
+  {
+    "id": 5,
+    "question": "What is the past continuous tense of 'sleep'?",
+    "answer": "was sleeping",
+    "options": ["slept","was sleeping",  "sleeping"]
+  },
+  {
+    "id": 6,
+    "question": "What is the past tense of 'read'?",
+    "answer": "read",
+    "options": [ "reads","read", "reading"]
+  },
+  {
+    "id": 7,
+    "question": "What is the present tense of 'write'?",
+    "answer": "write",
+    "options": ["write", "wrote", "written"]
+  },
+  {
+    "id": 8,
+    "question": "What is the future tense of 'sing'?",
+    "answer": "will sing",
+    "options": [ "sang", "singing","will sing"]
+  },
+  {
+    "id": 9,
+    "question": "What is the present perfect tense of 'do'?",
+    "answer": "have done",
+    "options": [ "did","have done", "done"]
+  },
+  {
+    "id": 10,
+    "question": "What is the past continuous tense of 'eat'?",
+    "answer": "was eating",
+    "options": [ "ate","was eating", "eating"]
+  }
+];
+List<Map<String, dynamic>> medium = [
+  {
+    "id": 1,
+    "question": "What is the past tense of 'speak'?",
+    "answer": "spoke",
+    "options": [ "speaking","spoke", "speaks"]
+  },
+  {
+    "id": 2,
+    "question": "What is the present continuous tense of 'run'?",
+    "answer": "is running",
+    "options": [ "ran", "running","is running"]
+  },
+  {
+    "id": 3,
+    "question": "What is the future tense of 'write'?",
+    "answer": "will write",
+    "options": ["will write", "wrote", "writing"]
+  },
+  {
+    "id": 4,
+    "question": "What is the present perfect tense of 'take'?",
+    "answer": "have taken",
+    "options": ["have taken", "took", "takes"]
+  },
+  {
+    "id": 5,
+    "question": "What is the past continuous tense of 'swim'?",
+    "answer": "was swimming",
+    "options": [ "swam", "swimming","was swimming"]
+  },
+  {
+    "id": 6,
+    "question": "What is the past tense of 'think'?",
+    "answer": "thought",
+    "options": [ "thinks", "thinking","thought"]
+  },
+  {
+    "id": 7,
+    "question": "What is the present tense of 'drive'?",
+    "answer": "drive",
+    "options": [ "drove","drive", "driving"]
+  },
+  {
+    "id": 8,
+    "question": "What is the future tense of 'dance'?",
+    "answer": "will dance",
+    "options": ["danced","will dance",  "dancing"]
+  },
+  {
+    "id": 9,
+    "question": "What is the present perfect tense of 'see'?",
+    "answer": "have seen",
+    "options": ["have seen", "saw", "seeing"]
+  },
+  {
+    "id": 10,
+    "question": "What is the past continuous tense of 'drink'?",
+    "answer": "was drinking",
+    "options": [ "drank", "drinking","was drinking"]
+  }
+];
+List<Map<String, dynamic>> expert = [
+  {
+    "id": 1,
+    "question": "What is the past tense of 'begin'?",
+    "answer": "began",
+    "options": ["began", "beginning", "begins"]
+  },
+  {
+    "id": 2,
+    "question": "What is the present continuous tense of 'fly'?",
+    "answer": "is flying",
+    "options": ["is flying", "flew", "flies"]
+  },
+  {
+    "id": 3,
+    "question": "What is the future tense of 'give'?",
+    "answer": "will give",
+    "options": [ "gave", "giving","will give"]
+  },
+  {
+    "id": 4,
+    "question": "What is the present perfect tense of 'fall'?",
+    "answer": "have fallen",
+    "options": ["fell", "falling","have fallen" ]
+  },
+  {
+    "id": 5,
+    "question": "What is the past continuous tense of 'fall'?",
+    "answer": "was falling",
+    "options": ["was falling", "fell", "falling"]
+  },
+  {
+    "id": 6,
+    "question": "What is the past tense of 'wear'?",
+    "answer": "wore",
+    "options": [ "wears","wore", "wearing"]
+  },
+  {
+    "id": 7,
+    "question": "What is the present tense of 'lead'?",
+    "answer": "lead",
+    "options": ["lead", "led", "leading"]
+  },
+  {
+    "id": 8,
+    "question": "What is the future tense of 'leave'?",
+    "answer": "will leave",
+    "options": [ "left","will leave", "leaving"]
+  },
+  {
+    "id": 9,
+    "question": "What is the present perfect tense of 'buy'?",
+    "answer": "have bought",
+    "options": ["have bought", "bought", "buying"]
+  },
+  {
+    "id": 10,
+    "question": "What is the past continuous tense of 'come'?",
+    "answer": "was coming",
+    "options": [ "came", "coming","was coming"]
+  }
+];
+
+

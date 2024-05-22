4578d8f3ddec24aebfc6a2ef0975c65df18da847
nasi321 <nasinasri6102@gmail.com>
2024-05-22 11:24:07 +0530

    modified

--- lib/presentation/games/tens_quiz/quiz datas.dart
+++
@@ -1,7 +0,0 @@
-List<Map<String,dynamic>> products=[{
-"id":1,
-"name":"product 1",
-"price": 900,
-"description":"Adidas shoes",
-"image":"https://m.media-amazon.com/images/G/31/img21/shoes/2023/SS23/Coop/Shoesupdated/Campus-978x1256_1._SY1050_QL85_.jpg",
-"rating":4.3},

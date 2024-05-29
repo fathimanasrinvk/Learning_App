f7ee6a81b7c99e2c19c2762001917fc81b77e8b9
nasi321 <nasinasri6102@gmail.com>
2024-05-29 12:39:47 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(
-        name: 'git',
-      ),
+      //home: HomeScreen(),
       //  home: PuzzleScreen()
       // home: LoginScreen()
-      // home:SplashScreen(),
+      home:SplashScreen(),
       //home:WordScreen(),
       //    home:RegistrationScreen(),
       //    home:OnboardingScreen(),
       //    home:TranslationScreen()
     );
   }
-}

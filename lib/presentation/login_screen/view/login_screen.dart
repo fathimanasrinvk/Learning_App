d08f6b58d6c0ca283b872a3a1c5ceda9e714b1a5
fathimathsafa <safahaneefa01@gmail.com>
2024-06-06 13:54:33 +0530

    login screen updated

--- lib/presentation/login_screen/view/login_screen.dart
+++ lib/presentation/login_screen/view/login_screen.dart
@@ -29,143 +29,146 @@ class _LoginScreenState extends State<LoginScreen> {
         body: SingleChildScrollView(
             child: Center(
                 child: Column(children: [
-                  SizedBox(
-                    height: size * 150,
+          SizedBox(
+            height: size * 150,
+          ),
+          Text("Welcome Back !!", style: GlobalTextStyles.secondTittle),
+          Padding(
+            padding: EdgeInsets.only(
+                left: size * 35, right: size * 35, top: size * 100),
+            child: TextFormField(
+              controller: namecontroller,
+              textInputAction: TextInputAction.next,
+              style: TextStyle(color: ColorTheme.maincolor),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: ColorTheme.maincolor,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Username',
+                contentPadding: EdgeInsets.symmetric(horizontal: size * 20),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide.none),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: size * 35, right: size * 35, top: size * 30),
+            child: TextFormField(
+              controller: pcontroller,
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: ColorTheme.maincolor,
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Password',
+                contentPadding: EdgeInsets.symmetric(horizontal: size * 20),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide.none),
+              ),
+              style: TextStyle(color: ColorTheme.maincolor),
+            ),
+          ),
+          SizedBox(
+            height: size * 40,
+          ),
+          Container(
+            width: size * 250,
+            decoration: BoxDecoration(
+                color: ColorTheme.maincolor,
+                borderRadius: BorderRadius.circular(size * 7)),
+            child: ElevatedButton(
+              onPressed: () async {
+                SharedPreferences preferences =
+                    await SharedPreferences.getInstance();
+                List<String>? userListJson = preferences.getStringList('users');
+                if (userListJson != null && userListJson.isNotEmpty) {
+                  List<User> userList = userListJson
+                      .map((userJson) => User.fromJson(json.decode(userJson)))
+                      .toList();
+                  String enteredName = namecontroller.text;
+                  String enteredPassword = pcontroller.text;
+                  User? user;
+                  try {
+                    user =
+                        userList.firstWhere((user) => user.name == enteredName);
+                  } catch (e) {
+                    user = null;
+                  }
+                  if (user != null && user.pword == enteredPassword) {
+                    await _writeName(enteredName);
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                    namecontroller.text = "";
+                    pcontroller.text = "";
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        backgroundColor: Colors.red,
+                        content: Text("Username or password is incorrect")));
+                  }
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text("No users registered yet")));
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorTheme.maincolor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7))),
+              child: Text("Login", style: GlobalTextStyles.buttonText),
+            ),
+          ),
+          SizedBox(height: size * 260),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => RegistrationScreen()));
+            },
+            child: RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "Don't have account ?  ",
+                    style: GoogleFonts.lato(
+                        decoration: TextDecoration.none,
+                        fontSize: size * 14,
+                        color: Colors.black),
                   ),
-                  Text("Welcome Back !!", style: GlobalTextStyles.secondTittle),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: size * 35, right: size * 35, top: size * 100),
-                    child: TextFormField(
-                      controller: namecontroller,
-                      textInputAction: TextInputAction.next,
-                      style: TextStyle(color: ColorTheme.maincolor),
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.person,
-                          color: ColorTheme.maincolor,
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Username',
-                        contentPadding: EdgeInsets.symmetric(horizontal: size * 20),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(7),
-                            borderSide: BorderSide.none),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: size * 35, right: size * 35, top: size * 30),
-                    child: TextFormField(
-                      controller: pcontroller,
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.lock,
-                          color: ColorTheme.maincolor,
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Password',
-                        contentPadding: EdgeInsets.symmetric(horizontal: size * 20),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(7),
-                            borderSide: BorderSide.none),
-                      ),
-                      style: TextStyle(color: ColorTheme.maincolor),
-                    ),
-                  ),
-                  SizedBox(
-                    height: size * 40,
-                  ),
-                  Container(
-                    width: size * 250,
-                    decoration: BoxDecoration(
+                  TextSpan(
+                    text: " Sign Up",
+                    style: GoogleFonts.lato(
                         color: ColorTheme.maincolor,
-                        borderRadius: BorderRadius.circular(size * 7)),
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        SharedPreferences preferences =
-                        await SharedPreferences.getInstance();
-                        List<String>? userListJson = preferences.getStringList('users');
-                        if (userListJson != null && userListJson.isNotEmpty) {
-                          List<User> userList = userListJson
-                              .map((userJson) => User.fromJson(json.decode(userJson)))
-                              .toList();
-                          String enteredName = namecontroller.text;
-                          String enteredPassword = pcontroller.text;
-                          User? user;
-                          try {
-                            user =
-                                userList.firstWhere((user) => user.name == enteredName);
-                          } catch (e) {
-                            user = null;
-                          }
-                          if (user != null && user.pword == enteredPassword) {
-                            await _writeName(enteredName);
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => HomeScreen()));
-                            namecontroller.text = "";
-                            pcontroller.text = "";
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                backgroundColor: Colors.red,
-                                content: Text("Username or password is incorrect")));
-                          }
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                              backgroundColor: Colors.red,
-                              content: Text("No users registered yet")));
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorTheme.maincolor,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7))),
-                      child: Text("Login", style: GlobalTextStyles.buttonText),
-                    ),
-                  ),
-                  SizedBox(height: size * 280),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => RegistrationScreen()));
-                    },
-                    child: RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: "Don't have account ?  ",
-                            style: GoogleFonts.lato(
-                                decoration: TextDecoration.none,
-                                fontSize: size * 14,
-                                color: Colors.black),
-                          ),
-                          TextSpan(
-                            text: " Sign Up",
-                            style: GoogleFonts.lato(
-                                color: ColorTheme.maincolor,
-                                decoration: TextDecoration.none,
-                                fontSize: size * 18,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                    ),
+                        decoration: TextDecoration.none,
+                        fontSize: size * 18,
+                        fontWeight: FontWeight.bold),
                   )
-                ]))));
+                ],
+              ),
+            ),
+          )
+        ]))));
   }
 }
+
 Future<File> _getLocalFile() async {
   final directory = await getApplicationDocumentsDirectory();
   return File('${directory.path}/name.txt');
 }
+
 Future<void> _writeName(String name) async {
   final file = await _getLocalFile();
   await file.writeAsString(name);
 }
+
 Future<String> _readName() async {
   try {
     final file = await _getLocalFile();

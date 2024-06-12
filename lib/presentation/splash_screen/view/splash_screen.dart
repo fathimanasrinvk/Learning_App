5d94c14eb016a4b06b2afab2d981ab317da1104d
fathimathsafa <safahaneefa01@gmail.com>
2024-06-12 13:57:29 +0530

    update

--- lib/presentation/splash_screen/view/splash_screen.dart
+++ lib/presentation/splash_screen/view/splash_screen.dart
@@ -13,7 +13,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   void initState() {
-    Timer(Duration(seconds: 10), () {
+    Timer(Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => OnboardingScreen()));
     });
